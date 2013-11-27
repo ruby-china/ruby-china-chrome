207035c945bfd5221d551a25f0f57b702042ff46
greatghoul <greatghoul@gmail.com>
2013-11-27 14:35:02 +0800

    鼠标移过名字时显示 popover

--- javascripts/contentscript.js
+++ javascripts/contentscript.js
@@ -1,9 +1,16 @@
 $(function() {
-	$(document).on('mouseover', '.face a, .avatar a, .info a[data-name], .at_user', function() {
-		var username = $(this).attr('href').substring(1);
+	$(document).on('mouseover', '.face a, .avatar a, .info a[data-name], .at_user', function(evt) {
+		var $link = $(this);
+		var username = $link.attr('href').substring(1);
 		$.getJSON('http://ruby-china.org/api/users/' + username + '.json', function(user) {
-			// TODO 通过 Popover 显示用户的详细信息
+			// 浮层中显示最近的话题
 			console.log(user);
+			$link.data('powertip', function() {
+				return 'ID: ' + user.login + '<br/>'
+					+ '城市：' + user.location + '<br/>'
+					+ '签名：' + user.tagline + '<br/>';
+			}).powerTip();
+			$.powerTip.show($link);
 		});
 	});
 });
