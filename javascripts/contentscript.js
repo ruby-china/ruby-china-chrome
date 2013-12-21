faf328bb9e0f4c3f8500ee1ebfb997e111fa616d
greatghoul <greatghoul@gmail.com>
2013-12-22 00:42:39 +0800

    删除 powertip
    依赖太多的，尽量避免使用不必要的扩展

--- javascripts/contentscript.js
+++ javascripts/contentscript.js
@@ -5,12 +5,12 @@ $(function() {
 		$.getJSON('http://ruby-china.org/api/users/' + username + '.json', function(user) {
 			// 浮层中显示最近的话题
 			console.log(user);
-			$link.data('powertip', function() {
-				return 'ID: ' + user.login + '<br/>'
-					+ '城市：' + user.location + '<br/>'
-					+ '签名：' + user.tagline + '<br/>';
-			}).powerTip();
-			$.powerTip.show($link);
+			// $link.data('powertip', function() {
+			// 	return 'ID: ' + user.login + '<br/>'
+			// 		+ '城市：' + user.location + '<br/>'
+			// 		+ '签名：' + user.tagline + '<br/>';
+			// }).powerTip();
+			// $.powerTip.show($link);
 		});
 	});
 });
