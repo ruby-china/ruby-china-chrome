1666ca1ff6d4bfc7b1588539852dab9b54630a59
greatghoul <greatghoul@gmail.com>
2013-12-23 10:18:11 +0800

    修正 jQuery 引用错误 #3
    之前将 jQuery 升级到了 2.0.3 但，漏掉了 popup 和 option 页面。

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -2,14 +2,12 @@ var rubyChinaApp = angular.module('rubyChinaApp', []);
 
 rubyChinaApp.filter('timeago', function() {
     return function(input) {
-        console.log(input);
         return $.timeago(input);
     }
 });
 
 rubyChinaApp.controller('TopicListCtrl', function TopicListCtrl($scope, $http) {
     $http.get('http://ruby-china.org/api/topics.json').success(function(data) {
-        console.log(JSON.stringify(data[1]));
         $scope.topics = data;
         $('#loading').hide();
         jQuery("abbr.timeago").timeago();
