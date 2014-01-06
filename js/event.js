039e696ec9cbe05789dd2ccc067f6646d9ed567c
greatghoul <greatghoul@gmail.com>
2014-01-06 17:33:32 +0800

    使用 alarms api 改写 event page #6
    alarms api 比  setInterval 更高效，当闲置时，event page
    会暂时销毁，节省资源。

--- js/event.js
+++ js/event.js
@@ -1,26 +1,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-
+    chrome.tabs.create({ url: 'http://ruby-china.org/notifications', active: false }); 
+    updateUnreadCount();
 });
 
-var fetching = false;
-var unread_count = 0;
-function fetchNotify(){
-    if(!fetching) {
-        fetching = true;
-        $.get("http://ruby-china.org/", function(r) {
-            unread_count = $(r).find("#userbar li a span").text();
-            if (unread_count == "0") {
-                chrome.browserAction.setBadgeText({'text' : ""});
-            } else {
-                chrome.browserAction.setBadgeText({'text' : unread_count});
-            }
+function updateUnreadCount() {
+    chrome.browserAction.setBadgeText({ text: localStorage['unread_notification_count'] || '' });
+    localStorage['unread_notification_count'] = '';
+}
 
-        });
-    }
-    fetching = false;
+function checkNewNotifications() {
+    $.get('http://ruby-china.org/wiki/about', function(content) {
+        var unread_count = parseInt($(content).find("#user_notifications_count .badge").text());
+        localStorage['unread_notification_count'] = unread_count || '';
+    });
 }
-fetchNotify();
-if(localStorage["update_duration"]==undefined){
-    localStorage["update_duration"] = 5;
-}
-setInterval(fetchNotify, 1000 * 60 * localStorage["update_duration"]);
+
+// TODO: 查阅资料，确定 alarms 创建的最合适的时机
+chrome.alarms.clearAll();
+// FIXME: 提醒时间先写死，后面再调整
+chrome.alarms.create('check new notifications', { periodInMinutes: 3 });
+chrome.alarms.onAlarm.addListener(checkNewNotifications);
